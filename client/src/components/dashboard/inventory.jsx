1e127e9abc0a587d81c34dde2fcebe2db901b889
Fifinixx <51245922+Fifinixx@users.noreply.github.com>
2025-09-30 14:57:15 +0530

    Update inventory.jsx
    Casing problem

--- client/src/components/dashboard/inventory.jsx
+++ client/src/components/dashboard/inventory.jsx
@@ -22,7 +22,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/Label";
+import { Label } from "@/components/ui/label";
 
 import { formatCurrency, capitalize } from "../../lib/utils";
 
