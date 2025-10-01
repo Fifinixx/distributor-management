4053175df29714b05577d3fd33a7261adb60b3b6
Fifinixx <51245922+Fifinixx@users.noreply.github.com>
2025-10-01 16:10:17 +0530

    Update performance.js

--- server/controllers/performance.js
+++ server/controllers/performance.js
@@ -5,33 +5,33 @@ async function FetchPerformance(req, res, next) {
   const oneYearAgo = new Date();
   oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
   try {
-      const purchases = await OrdersModel.aggregate([
-        { $match: { createdAt: { $gte: oneYearAgo } } },
-        {
-          $group: {
-            _id: {
-                 year: { $year: { date: "$createdAt", timezone: "Asia/Kolkata" } },
-      month: { $month: { date: "$createdAt", timezone: "Asia/Kolkata" } }
-            },
-              year: { $year: { date: "$createdAt", timezone: "Asia/Kolkata" } },
-      month: { $month: { date: "$createdAt", timezone: "Asia/Kolkata" } }
-          }
+    const purchases = await OrdersModel.aggregate([
+      { $match: { createdAt: { $gte: oneYearAgo } } },
+      {
+        $group: {
+          _id: {
+            year: { $year: { date: "$createdAt", timezone: "Asia/Kolkata" } },
+            month: { $month: { date: "$createdAt", timezone: "Asia/Kolkata" } },
+          },
+          totalPurchases: { $sum: "$grandTotal" },
+          totalDamageDiscount: { $sum: "$discountAmount" },
         },
-        { $sort: { "_id.year": 1, "_id.month": 1 } }
-      ]);
+      },
+      { $sort: { "_id.year": 1, "_id.month": 1 } },
+    ]);
     
     const sales = await SalesModel.aggregate([
       { $match: { createdAt: { $gte: oneYearAgo } } },
       {
         $group: {
           _id: {
-            year: { $year: "$createdAt" },
-            month: { $month: "$createdAt" }
+            year: { $year: { date: "$createdAt", timezone: "Asia/Kolkata" } },
+            month: { $month: { date: "$createdAt", timezone: "Asia/Kolkata" } },
           },
-          totalSales: { $sum: "$grandTotal" }
-        }
+          totalSales: { $sum: "$grandTotal" },
+        },
       },
-      { $sort: { "_id": 1, "_id": 1 } }
+      { $sort: { "_id.year": 1, "_id.month": 1 } },
     ]);
     return res.json({purchases, sales })
 
